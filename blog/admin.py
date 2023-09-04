5473743c358b0e466ca9c5459582d89397ee1d59
Emma Karlsson <emma94k@gmail.com>
2023-09-04 19:27:27 +0000

    build django admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    list_display = ('title', 'slug', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('description')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on')
+    search_fields = ('name', 'email', 'body')
