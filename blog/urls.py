9aee2f6c07f3b79260d333073b1d03114eb0caa6
Emma Karlsson <emma94k@gmail.com>
2023-09-05 22:18:26 +0000

    add html for delete post and update url

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('add_image/', views.AddImage.as_view(), name='add_image'),
     path('<slug:slug>/', views.FeaturedPost.as_view(), name='featured_post'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
-    path('delete_post/', views.DeletePost.as_view(), name='delete_post'),
+    path('<slug:slug>/delete_post/', views.DeletePost.as_view(), name='delete_post'),
     path('edit_post/', views.EditPost.as_view(), name='edit_post')
 ]
