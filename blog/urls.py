0429b5f9ac7c33f2195d718c371450affdf22d79
Emma Karlsson <emma94k@gmail.com>
2023-09-05 21:37:13 +0000

    add url for delete post

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('add_image/', views.AddImage.as_view(), name='add_image'),
     path('<slug:slug>/', views.FeaturedPost.as_view(), name='featured_post'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('delete_post/', views.DeletePost.as_view(), name='delete_post'),
 ]
