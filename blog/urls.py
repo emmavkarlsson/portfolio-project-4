fdfc3ccaea70907ed49ce4ccc7ef358b61c585ab
Emma Karlsson <emma94k@gmail.com>
2023-09-05 14:39:24 +0000

    add url for post likes

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.FeaturedPost.as_view(), name='featured_post'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
