964bac64404f1fb7441a15678c26c41f43be51b3
Emma Karlsson <emma94k@gmail.com>
2023-09-06 14:03:57 +0000

    add url for my posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('my_posts/', views.MyPosts.as_view(), name='my_posts'),
     path('add_image/', views.AddImage.as_view(), name='add_image'),
     path('<slug:slug>/', views.FeaturedPost.as_view(), name='featured_post'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
