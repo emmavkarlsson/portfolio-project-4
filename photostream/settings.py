fd3b0f3d23501244e7645f2124306d1d571bc996
Emma Karlsson <emma94k@gmail.com>
2023-09-10 05:15:08 +0000

    set debug to false in settings

--- photostream/settings.py
+++ photostream/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-emmavkarlss-portfoliopr-fnxbmyqs6n2.ws-eu104.gitpod.io', 'evk-photostream-d1447cc37dcb.herokuapp.com', 'localhost']  # noqa
 
